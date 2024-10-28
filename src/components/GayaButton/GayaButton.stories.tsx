c49c5b794ab3c436117e7bffdb65a6688c64189c
Vagner Barbosa <vagner.zaham@gmail.com>
2024-10-28 13:23:22 -0300

    feat: added GayaProvider

--- src/components/GayaButton/GayaButton.stories.tsx
+++ src/components/GayaButton/GayaButton.stories.tsx
@@ -1,35 +1,44 @@
-import React from 'react'
-import type { Meta, StoryObj } from '@storybook/react';
-// import { fn } from '@storybook/test';
+import React from 'react';
+import type { Meta, StoryObj, StoryFn, Decorator } from '@storybook/react';
 import { GayaButtonBase } from './GayaButton';
+import { GayaProvider } from '../../common/GayaProvider';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
-const meta = {
+const meta: Meta<typeof GayaButtonBase> = {
   title: 'GaYa Button',
   component: GayaButtonBase,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  //   args: { onClick: fn() },
-} satisfies Meta<typeof GayaButtonBase>;
+};
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
     onPress: () => {
-      ('');
+      // Função vazia ou implementação desejada
     },
     text: 'GayaButton',
     iconName: 'outlined-social-sparks',
     iconPosition: 'left',
   },
+};
+
+const withCustomTheme: Decorator = (Story) => (
+  <GayaProvider brand='avon_v2' mode='light'>
+    <Story />
+  </GayaProvider>
+);
+
+export const StoryComTemaPersonalizado = (args: React.ComponentProps<typeof GayaButtonBase>) => (
+  <GayaButtonBase {...args} />
+);
+StoryComTemaPersonalizado.decorators = [withCustomTheme];
+StoryComTemaPersonalizado.args = {
+  onPress: undefined,
+  text: 'GayaButton',
+  iconName: 'outlined-social-sparks',
+  iconPosition: 'left',
 };
