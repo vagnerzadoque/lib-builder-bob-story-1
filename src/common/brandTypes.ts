718a037a5a765794542f63bdfccbe107ef8ac760
Vagner Barbosa <vagner.zaham@gmail.com>
2024-11-15 17:52:44 -0300

    fix: update toolbar in previews file

--- src/common/brandTypes.ts
+++ src/common/brandTypes.ts
@@ -11,3 +11,18 @@ export type BrandTypes =
   | 'natura_v3'
   | 'casaEestilo'
   | 'casaEestilo_v2';
+
+
+export const brands = [
+  { name: "Aesop", value: "aesop"},
+  { name: "Avon", value: "avon"},
+  { name: "Avon-V2", value: "avon_v2"},
+  { name: "Natura", value: "natura"},
+  { name: "Natura-V2", value: "natura_v2"},
+  { name: "Natura-V3", value: "natura_v3"},
+  { name: "The Body Shop", value: "theBodyShop"},
+  { name: "Consultoria de Beleza", value: "consultoriaDeBeleza"},
+  { name: "Força de Vendas", value: "forcaDeVendas"},
+  { name: "Casa & Estilo", value: "casaEestilo"},
+  { name: "Casa & Estilo-V2", value: "casaEestilo_v2"},
+]
