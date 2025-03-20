f190249c22cf8fe43b652c52d9b901aa6b9cd59c
Vagner Barbosa <vagner.zaham@gmail.com>
2025-03-20 12:18:43 -0300

    fix: cleaning comments

--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -7,7 +7,7 @@ import { View } from 'react-native'
 import { BrandTypes, brands } from '../../common/brandTypes';
 
 const description = ` [Acesse a documentação completa no Confluence.](https://natura.atlassian.net/wiki/spaces/NatDS/pages/5475631142/Figma+gayaBadge) 
-> Badge is a visual indicator for numeric values.
+
 
 
 
